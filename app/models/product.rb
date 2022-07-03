02ca55ff876c339b39f0d48a8cea10d1d7b0eb39
arthurariza <tucaariza@gmail.com>
2022-07-03 20:37:47 -0300

    feat: product validations
    • The title, description, and image URL fields aren’t empty. • The price is a valid number not less than $0.01.
    • The title is unique among all products.
    • The image URL looks reasonable.

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,11 @@
 class Product < ApplicationRecord
+  validates :title, :description, :image_url, presence: true
+  validates :title, uniqueness: true
+
+  # Why test against one cent, rather than zero? Well, it’s possible to enter a number such as 0.001 into this field.
+  # Because the database stores just two digits after the decimal point, this would end up being zero in the database
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+
+  validates :image_url, allow_blank: true, format: { with: /\.(gif|jpg|png)\z/i,
+                                                     message: 'must be a URL for GIF, JPG or PNG image.' }
 end
