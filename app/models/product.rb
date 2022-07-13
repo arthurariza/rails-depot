e26ad47f60668900ee2363afe1adacfb6ae6cd25
arthurariza <tucaariza@gmail.com>
2022-07-12 23:40:01 -0300

    feat: add connecting products to cart
    product has many line items
    
    product cant be deleted if referenced by line item

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,6 @@
 class Product < ApplicationRecord
+  has_many :line_items
+
   validates :title, :description, :image_url, presence: true
   validates :title, uniqueness: true
 
@@ -8,4 +10,15 @@ class Product < ApplicationRecord
 
   validates :image_url, allow_blank: true, format: { with: /\.(gif|jpg|png)\z/i,
                                                      message: 'must be a URL for GIF, JPG or PNG image.' }
+
+  before_destroy :ensure_not_referenced_by_any_line_item
+
+  private
+
+  def ensure_not_referenced_by_any_line_item
+    unless line_items.empty?
+      errors.add(:base, 'Line Item Present')
+      throw :abort
+    end
+  end
 end
